426b510bbddcc4bf1e7cdefa04582a346b4a4c6c
Frans <frans.rastas@metropolia.fi>
2026-01-19 11:19:46 +0200

    lab5 done

---
+++ week2/week2-fe-lab5/src/Footer.jsx
@@ -0,0 +1,13 @@
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer">
+    <p>Copyright © 2026 My JSX App</p>
+    <p>Design</p>
+    </div>
+  );
+}
+
+export default Footer;
